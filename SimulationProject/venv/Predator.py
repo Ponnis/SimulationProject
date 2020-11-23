c624a148cbc0ea9ef541b1ce732eff3a533ac113
tiagoespsanto <60974869+tiagoespsanto@users.noreply.github.com>
2020-11-23 23:22:56 +0100

    Lots of questions regarding these two

--- SimulationProject/venv/Predator.py
+++ SimulationProject/venv/Predator.py
@@ -1,7 +1,13 @@
 class Predator:
-    # Code for predator goes here
-    attribute1 = 10  # TODO
-    attribute2 = 20  # TODO
+    # parameters that are common to the movement of all the preys: speed, field of view (angle), position(x,y,z?)
+    # direction of movement(two angles [if we use 3d], gamma and phi?)
+    speed = 10  # TODO
+    field = 20  # TODO
+    pos_x = 0
+    pos_y = 0
+    pos_z = 0
+    gamma = 0 #angle for x-y plane
+    phi = 0 #angle for x-y plane
 
     def locatePrey(self):
         return self
--- SimulationProject/venv/Prey.py
+++ SimulationProject/venv/Prey.py
@@ -1,7 +1,14 @@
 class Prey:
     # Code for prey goes here
-    attribute1 = 10  # TODO
-    attribute2 = 20  # TODO
+    # parameters that are common to all the preys: speed, field of vision (angle), position(x,y,z?)
+    # direction of movement(two angles [if we use 3d], gamma and phi?)
+    speed = 10  # TODO
+    field = 20  # TODO
+    pos_x = 0
+    pos_y = 0
+    pos_z = 0
+    gamma = 0 #angle for x-y plane
+    phi = 0 #angle for x-y plane
 
     def checkForCollision(self):
         return self
